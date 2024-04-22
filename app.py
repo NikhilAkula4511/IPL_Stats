d33084c4419c9dacac1999a2d806142a16e5f811
NikhilAkula4511 <105285055+NikhilAkula4511@users.noreply.github.com>
2024-04-22 20:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,4 @@ st.title("Analysis of IPL stats ")
 st.sidebar.success("Select a page above.")
 st.header("IPL Logo")
 st.image(r'IPl_logo.png')
-st.header("List OFF IPL Teams")
-st.image(r'IPL_Teams.jpg ')
-st.header("A SnapShot of Player shot")
 
-st.image(r'IPL_stats\Rohit-Sharma-1-1.jpg')
