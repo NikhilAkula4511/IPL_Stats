5a056598f953978d3a4bcd740345ded0aef2aea7
NikhilAkula4511 <105285055+NikhilAkula4511@users.noreply.github.com>
2024-04-22 20:32:44 +0530

    IPL

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title="Ipl Statistics",
+    page_icon="🏏🏏",
+)
+st.header("IPL_Nikhil_Elite13")
+st.title("Analysis of IPL stats ")
+st.sidebar.success("Select a page above.")
+st.header("IPL Logo")
+st.image(r'C:\Users\Hello\Desktop\Streamlit\IPL_stats\IPl_logo.png')
+st.header("List OFF IPL Teams")
+st.image(r'C:\Users\Hello\Desktop\Streamlit\IPL_stats\IPL_Teams.jpg ')
+st.header("A SnapShot of Player shot")
+
+st.image(r'C:\Users\Hello\Desktop\Streamlit\IPL_stats\Rohit-Sharma-1-1.jpg')
